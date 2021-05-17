3fc05e9e1fe8990d834c5941cfc9a851cc5e3193
Sasha <lendiel2002@gmail.com>
2021-05-17 14:43:54 +0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
-
+  resources :users
 
   get 'static_pages/home'
   get 'static_pages/help'
